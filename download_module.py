fbfb27e9527ec974e62c75c7b99682e5bca65e9e
Stanislav Arnaudov <arnaud@arnaudDev.teco.edu>
2018-05-14 13:23:42 +0200

    Managing to tame html, dates, links, regex and... stuff!

--- download_module.py
+++ download_module.py
@@ -6,10 +6,14 @@ import utils as ut
 import sys
 import json
 import numpy as np
+from bs4 import BeautifulSoup
+import urllib
+import re
 
 
 base_url =  None
-
+start_date = None
+end_date = None
 
 
 def main():
@@ -20,8 +24,19 @@ def main():
     base_url = config_data["download_module"]["base_url"]#
     print(base_url)
 
+    html = urllib.request.urlopen("https://archive.luftdaten.info/2017-01-01/").read()
+    soup = BeautifulSoup(html, "lxml")
+
+    
+    for link in soup.findAll("a"):
+        if re.match(".*.csv", link.get("href")):
+            print( link.get("href"))
+            
+
+        
+    
     dates = np.arange(np.datetime64('2017-01-01'), np.datetime64('2017-12-31'))
-    print(dates)
+    # print(dates)
     
     
 if __name__ == '__main__':
